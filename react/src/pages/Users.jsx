06ed4264d84e4f3d9d75611107391c9f2760a590
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-16 20:05:21 +0600

    users crud api

--- react/src/pages/Users.jsx
+++ react/src/pages/Users.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
 
 export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient
+            .get("/users")
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    };
+
     return <div>Users</div>;
 }
