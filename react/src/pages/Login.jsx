f1ffa299584168acd1d700fadb9800ddb4d72477
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-16 13:10:20 +0600

    login signup page

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-    return <div>Login</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Login</h1>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not Registered?{" "}
+                        <Link to="/signup">Create an account</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-    return <div>Signup</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Signup</h1>
+                    <input type="text" placeholder="Full Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Confirm Password" />
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Already Registered? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
