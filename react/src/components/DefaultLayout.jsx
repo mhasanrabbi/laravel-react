2d9650f26e49fb7b1cc078eb0ef48f97fb8edf00
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-16 12:22:18 +0600

    protected routes with react

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
 
 export default function DefaultLayout() {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
     return (
         <div>
             <Outlet />
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import App from "./App";
 import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ContextProvider>
+            <RouterProvider router={router} />
+        </ContextProvider>
     </React.StrictMode>
 );
